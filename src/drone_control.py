2c2147c33cef1ec41751d1c0e5b0563654539398
Daniel Jiang <djia3099@uni.sydney.edu.au>
2020-05-09 16:26:01 +1000

    Initialised subs/pubs w/o callbacks

--- src/drone_control.py
+++ src/drone_control.py
@@ -10,10 +10,48 @@ import numpy as np
 
 import sensor_msgs.msg
 import nav_msgs.msg
+import std_msgs
+from mtrx_major.msg import Navdata
 from geometry_msgs.msg import Twist, Vector3, Pose, PoseWithCovariance, Point, Quaternion
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu, Image, CompressedImage
 
-import Navdata.msg
+class DroneX():
+    # In the initialisation:
+    # - Set up subscribers and publishers
+    # - Zero all velocities
+    def __init__(self):
+        print('Initialising a drone...')
+        self.odomOffset = None
+        self.pos = None
+        self.orient = None
+        self.battery = 100
+
+        # Define subscribers
+        odomSub = rospy.Subscriber("ardrone/odometry", nav_msgs.msg.Odometry, self.odom_callback, queue_size=100)
+        navdataSub = rospy.Subscriber("/ardrone/navdata", Navdata, self.navdata_callback, queue_size=100)
+
+        # Define publishers
+        takeoffPub = rospy.Publisher('/ardrone/takeoff', std_msgs.msg.Empty, queue_size=10)
+        landingPub = rospy.Publisher('ardrone/land', std_msgs.msg.Empty, queue_size=10)
+        estopPub = rospy.Publisher('ardrone/reset', std_msgs.msg.Empty, queue_size=10)
+        
+        zeroOdomPub = rospy.Publisher('dronex/odom', nav_msgs.Odometry, queue_size=100)
+        commandPub = rospy.Publisher('cmd_vel', geometry_msgs.msg.Twist, queue_size=100)    
+
+        
 
 
+    def odom_callback(self, odomMsg):
+        print("Stub")
+        return
+
+    def navdata_callback(self, navdataMsg):
+        print("Stub")
+        return
+
+if __name__ == '__main__':
+    print('Drone Control started...')
+    rospy.init_node('drone_control')
+
+    Drone = DroneX()
